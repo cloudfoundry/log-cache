a0cb986526edd1411fdd6f7e9466c87c8155c3db
Warren Fernandes <warren.f.fernandes@gmail.com>
2018-04-06 15:20:01 -0600

    Adds pprof support to log-cache-gateway
    Closes #41

--- cmd/gateway/config.go
+++ cmd/gateway/config.go
@@ -7,17 +7,18 @@ import (
 
 // Config is the configuration for a LogCache Gateway.
 type Config struct {
-	Addr         string `env:"ADDR, required"`
-	LogCacheAddr string `env:"LOG_CACHE_ADDR, required"`
-	TLS          tls.TLS
-
+	Addr            string `env:"ADDR, required"`
+	LogCacheAddr    string `env:"LOG_CACHE_ADDR, required"`
 	GroupReaderAddr string `env:"GROUP_READER_ADDR, required"`
+	HealthPort      int    `env:"HEALTH_PORT"`
+	TLS             tls.TLS
 }
 
 // LoadConfig creates Config object from environment variables
 func LoadConfig() (*Config, error) {
 	c := Config{
 		Addr:            ":8081",
+		HealthPort:      6063,
 		LogCacheAddr:    "localhost:8080",
 		GroupReaderAddr: "localhost:8082",
 	}
