bfd08c7e7681d84e5c25ef970530821b93679640
Todd Persen <tpersen@pivotal.io>
2018-08-23 09:51:49 -0600

    Correctly use the WithNozzleSelectors in nozzle
    Signed-off-by: Jesse Weaver <jeweaver@pivotal.io>

--- cmd/nozzle/main.go
+++ cmd/nozzle/main.go
@@ -62,7 +62,7 @@ func main() {
 				cfg.LogCacheTLS.Credentials("log-cache"),
 			),
 		),
-		logcache.WithSelectors(cfg.Selectors...),
+		logcache.WithNozzleSelectors(cfg.Selectors...),
 	)
 
 	go nozzle.Start()
