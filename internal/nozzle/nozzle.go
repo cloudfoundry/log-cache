c916ea211d263a099ecfeffac9ca638b40e7504f
Todd Persen <tpersen@pivotal.io>
2019-01-15 10:25:47 -0800

    Refactor nozzle to handle data more efficiently
    Signed-off-by: Hunter Williams <huwilliams@pivotal.io>

--- internal/nozzle/nozzle.go
+++ internal/nozzle/nozzle.go
@@ -3,8 +3,10 @@ package nozzle
 import (
 	"io/ioutil"
 	"log"
+	"runtime"
 	"time"
 
+	diodes "code.cloudfoundry.org/go-diodes"
 	"code.cloudfoundry.org/go-loggregator"
 	"code.cloudfoundry.org/go-loggregator/rpc/loggregator_v2"
 	"code.cloudfoundry.org/log-cache/internal/metrics"
@@ -15,16 +17,22 @@ import (
 
 // Nozzle reads envelopes and writes them to LogCache.
 type Nozzle struct {
-	log       *log.Logger
-	s         StreamConnector
-	metrics   metrics.Initializer
-	shardId   string
-	selectors []string
+	log          *log.Logger
+	s            StreamConnector
+	metrics      metrics.Initializer
+	shardId      string
+	selectors    []string
+	streamBuffer *diodes.OneToOne
 
 	// LogCache
 	addr string
 	opts []grpc.DialOption
 }
+
+const (
+	BATCH_FLUSH_INTERVAL = 500 * time.Millisecond
+	BATCH_CHANNEL_SIZE   = 512
+)
 
 // StreamConnector reads envelopes from the the logs provider.
 type StreamConnector interface {
@@ -47,6 +55,10 @@ func NewNozzle(c StreamConnector, logCacheAddr string, shardId string, opts ...N
 	for _, o := range opts {
 		o(n)
 	}
+
+	n.streamBuffer = diodes.NewOneToOne(100000, diodes.AlertFunc(func(missed int) {
+		n.log.Printf("Stream buffer dropped %d points", missed)
+	}))
 
 	return n
 }
@@ -99,6 +111,18 @@ func (n *Nozzle) Start() {
 	egressInc := n.metrics.NewCounter("Egress")
 	errInc := n.metrics.NewCounter("Err")
 
+	go n.envelopeReader(rx, ingressInc)
+
+	ch := make(chan []*loggregator_v2.Envelope, BATCH_CHANNEL_SIZE)
+
+	log.Printf("Starting %d nozzle workers...", runtime.NumCPU())
+	for i := 0; i < 2*runtime.NumCPU(); i++ {
+		go n.envelopeWriter(ch, client, errInc, egressInc)
+	}
+
+	// The batcher will block indefinitely.
+	n.envelopeBatcher(ch)
+
 	for {
 		batch := rx()
 		ingressInc(uint64(len(batch)))
@@ -117,6 +141,74 @@ func (n *Nozzle) Start() {
 		}
 
 		egressInc(uint64(len(batch)))
+	}
+}
+
+func (n *Nozzle) envelopeBatcher(ch chan []*loggregator_v2.Envelope) {
+	poller := diodes.NewPoller(n.streamBuffer)
+	envelopes := make([]*loggregator_v2.Envelope, 0)
+	t := time.NewTimer(BATCH_FLUSH_INTERVAL)
+	for {
+		data, found := poller.TryNext()
+
+		if found {
+			// this bizarre typecasting is an implementation detail of diode
+			envelope := *(*loggregator_v2.Envelope)(data)
+			envelopes = append(envelopes, &envelope)
+		}
+
+		select {
+		case <-t.C:
+			if len(envelopes) > 0 {
+				select {
+				case ch <- envelopes:
+					envelopes = make([]*loggregator_v2.Envelope, 0)
+				default:
+					envelopes = envelopes[:0]
+				}
+			}
+			t.Reset(BATCH_FLUSH_INTERVAL)
+		default:
+			if len(envelopes) >= BATCH_CHANNEL_SIZE {
+				select {
+				case ch <- envelopes:
+					envelopes = make([]*loggregator_v2.Envelope, 0)
+				default:
+					envelopes = envelopes[:0]
+				}
+				t.Reset(BATCH_FLUSH_INTERVAL)
+			}
+		}
+	}
+}
+
+func (n *Nozzle) envelopeWriter(ch chan []*loggregator_v2.Envelope, client logcache_v1.IngressClient, errInc, egressInc func(uint64)) {
+	for {
+		envelopes := <-ch
+
+		ctx, _ := context.WithTimeout(context.Background(), 3*time.Second)
+		_, err := client.Send(ctx, &logcache_v1.SendRequest{
+			Envelopes: &loggregator_v2.EnvelopeBatch{
+				Batch: envelopes,
+			},
+		})
+
+		if err != nil {
+			errInc(1)
+			continue
+		}
+
+		egressInc(uint64(len(envelopes)))
+	}
+}
+
+func (n *Nozzle) envelopeReader(rx loggregator.EnvelopeStream, ingressInc func(uint64)) {
+	for {
+		envelopeBatch := rx()
+		for _, envelope := range envelopeBatch {
+			n.streamBuffer.Set(diodes.GenericDataType(envelope))
+			ingressInc(1)
+		}
 	}
 }
 
